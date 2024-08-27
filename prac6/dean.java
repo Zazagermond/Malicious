52fb1a4e25a83f341d2dbab08ec22b3aee841577
Zazagermond <112818988+Zazagermond@users.noreply.github.com>
2024-08-27 17:24:47 +0200

    Question 4

--- prac6/dean.java
+++ prac6/dean.java
@@ -7,31 +7,42 @@ package EBNF;
 
 import java.util.*;
 import library.*;
-
+  
   class Entry {                      // Cross reference table entries
     public String name;              // The identifier itself
     public ArrayList<Integer> refs;  // Line numbers where it appears
-    public Entry(String name) {
+    public Entry next;
+    public Entry(){
+      this.next=null;
+    }
+    public Entry(String name, Entry rem) {
       this.name = name;
       this.refs = new ArrayList<Integer>();
+      if (rem==null){
+      this.next=null;}else {
+        this.next=rem;
+      }
     }
   } // Entry
 
   class Table {
-    ArrayList<Entry> Tableentres = new ArrayList<Entry>();
+    //ArrayList<Entry> Tableentres = new ArrayList<Entry>();//
+    public static Entry Tableentres;
     public static void clearTable() {
     // Clears cross-reference table
-    Tableentres.clear();
+    Tableentres=new Tableentres();
     } // clearTable
 
     public static void addRef(String name, boolean declared, int lineRef) {
     // What do you suppose is the purpose of the "declared" parameter?
     Entry test=getEntry(name);
+    
     if (test!=null) {
       Entry test=getEntry(name);
       (test.refs).add(lineRef); //does this change in tableenteis
     }else{
-    Tableentres.add(Entry(name)); //not sure how it handes lineref
+
+    Tableentres.add(Entry(name), null); //not sure how it handes lineref
     }
     } // addRef
 
@@ -47,7 +58,7 @@ import library.*;
       return null;
     }
 
-    public static void contains (String name){
+    public static boolean contains (String name){
       for (Entry aent: Tableentres){
         if ((aent.name).equals(aname))
         { return true;}
