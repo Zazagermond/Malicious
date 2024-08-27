8c15e297a663845dc6094aaa5909c62e83fd7324
thepineappleandthepie <100419298+thepineappleandthepie@users.noreply.github.com>
2024-08-27 15:39:21 +0200

    for rac 6 just testing thingy

---
+++ Table.java
@@ -0,0 +1,49 @@
+// Compute medal tallies for countries in Olympics productions
+// K. Bradshaw, Rhodes University, 2024
+
+// Complete this skeleton for Task 3 Prac 6, or develop your own version
+
+package Olympics;
+
+import java.util.*;
+import library.*;
+
+class Entry { // Country table entries
+  public String name; // The country name itself
+  public int total; // Total medals
+
+  public Entry(String name) {
+    this.name = name;
+    this.total = 0; // no medals yet
+  }
+} // Entry
+
+class Table {
+
+  private static Map<String, Entry> countryTable = new HashMap<>();  
+  public static void clearTable() {
+    // Clears cross-reference table
+    countryTable.clear();
+  } // clearTable
+
+  public static void incMedal(String name) {
+    // Increment the medal total for country "name" -- if country doesn't exist
+    // create it.
+
+    Entry entry = countryTable.get(name); // Check if the country already exists in the table
+    if (entry == null) { // If not, create a new entry
+      entry = new Entry(name);
+      countryTable.put(name, entry);
+    }
+    entry.total++; // Increment the medal count
+  } // incMedal
+
+  public static void printTable() {
+    // Prints out all countries with their medal totals
+    IO.writeLine("Country         Total medals");
+    for (Entry entry : countryTable.values()) {
+      Out.format("%-15s %d%n", entry.name, entry.total);
+    }
+  } // printTable
+
+} // Table
