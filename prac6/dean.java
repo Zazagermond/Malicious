b7847d3b730154c1de277906607b54230c6976f7
Dean <100418667+dshumbanhete@users.noreply.github.com>
2024-08-28 10:14:26 +0200

    Update dean.java needs testing

--- prac6/dean.java
+++ prac6/dean.java
@@ -7,64 +7,47 @@ package EBNF;
 
 import java.util.*;
 import library.*;
-  
+
   class Entry {                      // Cross reference table entries
     public String name;              // The identifier itself
     public ArrayList<Integer> refs;  // Line numbers where it appears
-    public Entry next;
-    public Entry(){
-      this.next=null;
-    }
-    public Entry(String name, Entry rem) {
+    public Entry(String name) {
       this.name = name;
       this.refs = new ArrayList<Integer>();
-      if (rem==null){
-      this.next=null;}else {
-        this.next=rem;
-      }
     }
   } // Entry
 
   class Table {
-    //ArrayList<Entry> Tableentres = new ArrayList<Entry>();//
-    public static Entry Tableentres;
+    static ArrayList<Entry> SymTable=new ArrayList<>();
     public static void clearTable() {
     // Clears cross-reference table
-    Tableentres=new Tableentres();
+      SymTable.clear();
     } // clearTable
 
     public static void addRef(String name, boolean declared, int lineRef) {
     // What do you suppose is the purpose of the "declared" parameter?
-    Entry test=getEntry(name);
-    
-    if (test!=null) {
-      Entry test=getEntry(name);
-      (test.refs).add(lineRef); //does this change in tableenteis
-    }else{
-
-    Tableentres.add(Entry(name), null); //not sure how it handes lineref
-    }
+      if (declared==true){
+        for (Entry i:SymTable){
+          if (i.name.equals(name))
+              i.refs.add(lineRef);
+      }}else{
+        Entry clone=new Entry(name);
+        clone.refs.add(0-lineRef);
+        SymTable.add(clone);
+      }
     } // addRef
 
     public static void printTable() {
     // Prints out all references in the table (eliminate duplicates line numbers)
-
+    String outer;
+    String roller="";
+      for (Entry i:SymTable){
+        for (int refer:i.refs){
+          roller=roller+"   "+refer;
+        }
+        outer=String.format("%s %6s %s", i.name,roller);
+        System.out.println(outer);
+      }
     } // printTable
-    public static void getEntry( String aname){
-      for (Entry aent: Tableentres){
-        if ((aent.name).equals(aname))
-        { return aent;}
-      }
-      return null;
-    }
-
-    public static boolean contains (String name){
-      for (Entry aent: Tableentres){
-        if ((aent.name).equals(aname))
-        { return true;}
-      }
-      return false;
-    }
-    
 
   } // Table
